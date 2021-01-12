feeefe75946fbc6bffc1aebb3167a9e9524be64e
KelipuTe <xhy_1365@sina.com>
2021-01-12 22:37:03 +0800

    pai2xu4

---
+++ suan_fa/cha_ru_pai_xu.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int daiPaiXuArr[10];
+    int daiPaiXuLen = 10;
+    srand(time(NULL));
+    for (int i = 0; i < daiPaiXuLen; i++)
+    {
+        daiPaiXuArr[i] = rand() % 100;
+    }
+    printf("pai2xu4qian2:");
+    shuChuXuLie(&daiPaiXuArr, daiPaiXuLen);
+    // 认为第一个元素有序，排序从第二个元素开始
+    for (int i = 1; i < daiPaiXuLen; i++)
+    {
+        int j = i;
+        // 待排序元素
+        int tNum = daiPaiXuArr[j];
+        while (j > 0 && tNum < daiPaiXuArr[j - 1])
+        {
+            // 如果比前面小，就往前移动，直到移动到第一位
+            daiPaiXuArr[j] = daiPaiXuArr[j - 1];
+            j--;
+        }
+        // 插入有序序列
+        daiPaiXuArr[j] = tNum;
+    }
+    printf("pai2xu4hou4:");
+    shuChuXuLie(&daiPaiXuArr, daiPaiXuLen);
+    return 0;
+}
+
+void shuChuXuLie(int *daiPaiXuArr, int daiPaiXuLen)
+{
+    for (int i = 0; i < daiPaiXuLen; i++)
+    {
+        printf("%d,", daiPaiXuArr[i]);
+    }
+    printf("\n");
+}
---
+++ suan_fa/mao_pao_pai_xu.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int daiPaiXuArr[10];
+    int daiPaiXuLen = 10;
+    srand(time(NULL));
+    for (int i = 0; i < daiPaiXuLen; i++)
+    {
+        daiPaiXuArr[i] = rand() % 100;
+    }
+    printf("pai2xu4qian2:");
+    shuChuXuLie(&daiPaiXuArr, daiPaiXuLen);
+    // 记录最后一个被交换的元素的位置
+    int lastExchangeIndex = daiPaiXuLen - 1;
+    for (int i = 0; i < daiPaiXuLen - 1; i++)
+    {
+        // 判断是否有元素交换
+        int isSorted = 0;
+        // 只需要比较到最后一个被交换的元素的位置
+        int lastSortedIndex = lastExchangeIndex;
+        for (int j = 0; j < lastSortedIndex; j++)
+        {
+            if (daiPaiXuArr[j] > daiPaiXuArr[j + 1])
+            {
+                int tNum = daiPaiXuArr[j];
+                daiPaiXuArr[j] = daiPaiXuArr[j + 1];
+                daiPaiXuArr[j + 1] = tNum;
+                isSorted = 1;
+                lastExchangeIndex = j;
+            }
+        }
+        if (isSorted == 0)
+        {
+            // 如果一轮排序没有元素被交换，则序列已经有序
+            break;
+        }
+    }
+    printf("pai2xu4hou4:");
+    shuChuXuLie(&daiPaiXuArr, daiPaiXuLen);
+    return 0;
+}
+
+void shuChuXuLie(int *daiPaiXuArr, int daiPaiXuLen)
+{
+    for (int i = 0; i < daiPaiXuLen; i++)
+    {
+        printf("%d,", daiPaiXuArr[i]);
+    }
+    printf("\n");
+}
