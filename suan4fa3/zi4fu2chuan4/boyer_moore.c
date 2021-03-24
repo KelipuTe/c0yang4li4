38c769ca128a4c8c8c9006602d730614366bb283
KelipuTe <xhy_1365@sina.com>
2021-03-24 22:53:25 +0800

    shu4ju4jie2gou4

--- suan4fa3/zi4fu2chuan4/boyer_moore.c
+++ suan4fa3/zi4fu2chuan4/boyer_moore.c
@@ -11,35 +11,34 @@
 
 int main() {
     // 匹配串，模式串
-    char sPiPei[] = "abcacabdabcabab", sMoSHi[] = "dabcab";
+    char sPiPei[] = "abcadabcabdababdabcab", sMoShi[] = "dabcab";
     // 匹配串长度，模式串长度
     int iSPiPeiLen = 0, iSMoSHiLen = 0;
-    // 坏字符
+    // 坏字符规则
     int iarrHuaiZiFu[256] = {0};
-    // 好后缀
+    // 好后缀规则
     // 模式串后缀在模式串前面再次出现的位置，能和后缀匹配的前缀的长度
     int iarrHaoHouZhui[MO_SHI_CHUAN_LEN] = {0}, ibarrHaoQianZhui[MO_SHI_CHUAN_LEN] = {0};
 
     iSPiPeiLen = strlen(sPiPei);
-    iSMoSHiLen = strlen(sMoSHi);
+    iSMoSHiLen = strlen(sMoShi);
 
-    // 找到每个字符在模式串中最后出现的位置
+    // 坏字符规则，找到每个字符在模式串中最后出现的位置
     for (int ii = 0; ii < iSMoSHiLen; ii++) {
-        iarrHuaiZiFu[(int)sMoSHi[ii]] = ii;
+        iarrHuaiZiFu[(int)sMoShi[ii]] = ii;
     }
-    // 处理好后缀的两个数组
+    // 处理好后缀规则的两个数组
     // 初始化
     for (int ii = 0; ii < MO_SHI_CHUAN_LEN; ii++) {
         iarrHaoHouZhui[ii] = -1;
         ibarrHaoQianZhui[ii] = 0;
     }
+    // 依次比较的是从0到ii下标位置的前缀子串，所以长度-1
     for (int ii = 0; ii < iSMoSHiLen - 1; ii++) {
-        // 比较的是从0到ii下标位置的前缀子串，所以长度-1
+        // ij，公共后缀长度
         int ij = ii, ik = 0;
-        iarrHaoHouZhui[ik] = -1;
-        ibarrHaoQianZhui[ii] = 0;
         // 求模式串从0到ii下标位置的前缀子串和整个模式串的公共后缀
-        while (ij >= 0 && sMoSHi[ij] == sMoSHi[iSMoSHiLen - 1 - ik]) {
+        while (ij >= 0 && sMoShi[ij] == sMoShi[iSMoSHiLen - 1 - ik]) {
             ik++;
             iarrHaoHouZhui[ik] = ij;
             ij--;
@@ -51,41 +50,48 @@ int main() {
     }
     {
         int ii = 0;
+        // 当匹配串的子串长度小于模式串时，就不需要在匹配了
         while (ii <= iSPiPeiLen - iSMoSHiLen) {
             int ij = 0;
-            // 从末尾开始比较
+            // BM算法从模式串的末尾开始比较
             for (ij = iSMoSHiLen - 1; ij >= 0; ij--) {
-                if (sPiPei[ii + ij] != sMoSHi[ij]) {
+                if (sPiPei[ii + ij] != sMoShi[ij]) {
                     break;
                 }
             }
             if (ij < 0) {
-                // 匹配完成
+                // 匹配完成，如果只找一个，那么这里可以结束了
                 printf("%d\n", ii);
+                // 匹配位置后移，匹配下一处
                 ii++;
-                continue;
-            }
-            // 如果没匹配到，那么ij就是坏字符的位置
-            int ia = ij - iarrHuaiZiFu[(int)sPiPei[ii + ij]];
-            int ib = 0;
-            if (ij < iSMoSHiLen - 1) {
-                // ij往前移动了，证明后缀有一部分匹配上了
-                // 好后缀的长度
-                int ik = iSMoSHiLen - 1 - ij;
-                if (iarrHaoHouZhui[ik] != -1) {
-                    // 看看这部分后缀在模式串前面有没有
-                    ib = ij - iarrHaoHouZhui[ik] + 1;
-                } else {
-                    ib = iSMoSHiLen;
-                    // 看看这部分后缀里有没有满足条件的前缀
-                    for (int il = ij + 2; il <= iSMoSHiLen - 1; il++) {
-                        if (ibarrHaoQianZhui[iSMoSHiLen - il] == 1) {
-                            ib = il;
+            } else {
+                // 如果没匹配完成，那么ij就是坏字符的位置
+                // 确认坏字符规则以移动位数
+                int ia = ij - iarrHuaiZiFu[(int)sPiPei[ii + ij]];
+                // 确认好后缀规则的移动位数
+                int ib = 0;
+                if (ij < iSMoSHiLen - 1) {
+                    // ij往前移动了，证明后缀有一部分匹配上了
+                    // 完成匹配的后缀的长度
+                    int ik = iSMoSHiLen - 1 - ij;
+                    if (iarrHaoHouZhui[ik] != -1) {
+                        // 如果这部分后缀在模式串前面存在，就把匹配串移动到相应的对齐的位置
+                        ib = ij - iarrHaoHouZhui[ik] + 1;
+                    } else {
+                        // 如果找不到后缀，先认为要移动整个模式串的长度
+                        // 然后判断完成匹配的后缀有没有和模式串的前缀重合
+                        ib = iSMoSHiLen;
+                        // 如果这部分后缀(后缀里的子后缀串也可以)，能匹配到满足条件的前缀，就把匹配串移动到相应的对齐的位置
+                        for (int il = ij + 1; il <= iSMoSHiLen - 1; il++) {
+                            if (ibarrHaoQianZhui[iSMoSHiLen - il] == 1) {
+                                ib = il;
+                            }
                         }
                     }
                 }
+                // 选移动位数大的那个
+                ii = ii + fmax(ia, ib);
             }
-            ii = ii + fmax(ia, ib);
         }
     }
 }
--- suan4fa3/zi4fu2chuan4/rabin_karp.c
+++ suan4fa3/zi4fu2chuan4/rabin_karp.c
@@ -8,7 +8,7 @@ extern int haXiHanShu();
 
 int main() {
     // 匹配串，模式串
-    char sPiPei[] = "hello world", sMoShi[] = "ll";
+    char sPiPei[] = "abcadabcabdababdabcab", sMoShi[] = "dabcab";
     // 匹配串长度，模式串长度
     int iSPiPeiLen = 0, iSMoShiLen = 0;
     // 匹配串哈希值，模式串哈希值，
@@ -19,7 +19,8 @@ int main() {
     // 计算子串的哈希值
     iHaXiZi = haXiHanShu(sMoShi, 0, iSMoShiLen);
     // 依次截取匹配串中长度和模式串相同的子串，计算哈希值并进行比较
-    for (int ii = 0; ii < iSPiPeiLen - iSMoShiLen; ii++) {
+    // 注意这里是小于等于，处理一样长或者正好是后缀的情况
+    for (int ii = 0; ii <= iSPiPeiLen - iSMoShiLen; ii++) {
         iHaXiZhu = haXiHanShu(sPiPei, ii, ii + iSMoShiLen);
         if (iHaXiZhu == iHaXiZi) {
             // 如果哈希值相同，再校验一次，确认字符串是否相同
@@ -30,7 +31,7 @@ int main() {
                 }
             }
             if (ibCheck == 1) {
-                // 匹配完成
+                // 匹配完成，如果只找一个，那么这里可以结束了
                 printf("%d\n", ii);
             }
         }
