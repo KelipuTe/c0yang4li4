72c1655608da083f8f407062e7d8cdece326f367
kelipute <xhy_1365@sina.com>
2022-01-09 21:54:47 +0800

    review

--- demo_linux_c/signal/signal.c
+++ demo_linux_c/signal/signal.c
@@ -11,15 +11,15 @@ void signal_handler(int signal_no) {
 int main() {
   printf("[debug]:getpid()=%d\r\n", getpid());
 
-// signal(2)
-    // #include <signal.h>
-    // ANSI C signal handling，给信号量定义一个处理函数
-    //收到SIGINT时，执行处理函数，signal_handler
-  signal(SIGINT, signal_handler); 
+  // signal(2)
+  // #include <signal.h>
+  // ANSI C signal handling，给信号量定义一个处理函数
+  //收到SIGINT时，执行处理函数，signal_handler
+  signal(SIGINT, signal_handler);
   //收到SIGUSR1时，执行忽略动作，SIG_IGN
-  signal(SIGUSR1, SIG_IGN);      
-    //收到SIGUSR2时，执行默认动作，SIG_DFL 
-  signal(SIGUSR2, SIG_DFL);     
+  signal(SIGUSR1, SIG_IGN);
+  //收到SIGUSR2时，执行默认动作，SIG_DFL
+  signal(SIGUSR2, SIG_DFL);
 
   while (1) {
     printf("[debug]:getpid()=%d\r\n", getpid());
