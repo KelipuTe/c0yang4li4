0b3836aa5cf9ddeed5d0dfa97a4a70aad64f75ab
西柊慧音 <xhy_1365@sina.com>
2023-06-26 23:07:31 +0800

    memory struct

--- demo-in-linux/memory/struct.c
+++ demo-in-linux/memory/struct.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 struct Person {
   char *name;
@@ -9,35 +11,37 @@ struct Person {
 } Person;
 
 int main() {
-  printf("sizeof(Person) value=%ld\r\n", sizeof(struct Person));
+  printf("pid=%d\n", getpid());
 
-  struct Person a, *p1b;
+  struct Person s6a, *p7b;
 
-  a.name = (char *)malloc(sizeof(char) * 16);
-  memcpy(a.name, "aaa", sizeof("aaa"));
-  a.age = 18;
-  a.sex = 'f';
+  s6a.name = (char *)malloc(sizeof(char) * 16);
+  memcpy(s6a.name, "aaa", sizeof("aaa"));
+  s6a.age = 18;
+  s6a.sex = 'f';
 
-  printf("sizeof(a) value=%ld\r\n", sizeof(a));
-  printf("&a        value=%p\r\n", &a);
-  printf("&a.name   value=%p\r\n", &a.name);
-  printf("&a.age    value=%p\r\n", &a.age);
-  printf("&a.sex    value=%p\r\n", &a.sex);
+  printf("sizeof(s6a)=%ld\n", sizeof(s6a));
+  printf("&s6a=%p\n", &s6a);
+  printf("&s6a.name=%p\n", &s6a.name);
+  printf("s6a.name=%p\n", s6a.name);
+  printf("&s6a.age=%p\n", &s6a.age);
+  printf("&s6a.sex=%p\n", &s6a.sex);
 
-  p1b = (struct Person *)malloc(sizeof(struct Person));
-  p1b->name = (char *)malloc(sizeof(char) * 16);
-  memcpy(p1b->name, "bbb", sizeof("bbb"));
-  p1b->age = 18;
-  p1b->sex = 'f';
+  p7b = (struct Person *)malloc(sizeof(struct Person));
+  p7b->name = (char *)malloc(sizeof(char) * 16);
+  memcpy(p7b->name, "bbb", sizeof("bbb"));
+  p7b->age = 18;
+  p7b->sex = 'f';
 
-  printf("sizeof(p1b) value=%ld\r\n", sizeof(struct Person *));
-  printf("p1b          value=%p\r\n", p1b);
-  printf("&p1b->name   value=%p\r\n", &p1b->name);
-  printf("&p1b->age    value=%p\r\n", &p1b->age);
-  printf("&p1b->sex    value=%p\r\n", &p1b->sex);
+  printf("sizeof(p7b)=%ld\n", sizeof(p7b));
+  printf("p7b=%p\n", p7b);
+  printf("&p7b->name=%p\n", &p7b->name);
+  printf("p7b->name=%p\n", p7b->name);
+  printf("&p7b->age=%p\n", &p7b->age);
+  printf("&p7b->sex=%p\n", &p7b->sex);
 
-  free(p1b->name);
-  free(p1b);
+  free(p7b->name);
+  free(p7b);
 
   return 0;
 }
